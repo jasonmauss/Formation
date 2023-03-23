440159aba61b80ec0d493c44b2f753e878a97c42
Jason Mauss <jason.mauss@gmail.com>
2023-03-22 21:37:29 -0700

    Adding Solution for JavaScript

--- Problem_3_ReverseListGroups/JavaScript/Problem_3_ReverseListGroups.js
+++ Problem_3_ReverseListGroups/JavaScript/Problem_3_ReverseListGroups.js
@@ -7,8 +7,41 @@
    }
  }
 
-function reverseInGroupsOfK(head, k) {
-  /* your code here */
+const reverseInGroupsOfK = (head, k) => {
+  
+    // If the head node passed in is null, or it's only a single node
+    // or the value of k is 1, don't bother with any of this.
+    if(!head || !head.next || k === 1) return head;
+
+    // previousNode and currentNode will help us hold on to references
+    // while we swap things around. lastNode will hold on to our reference
+    // of the node that ends up at the end of our "swap". nodeCount will
+    // just be our counter for adhering to swapping 'k' number of times.
+    let previousNode = head;
+    let currentNode = previousNode.next;
+    previousNode.next = null;
+    let lastNode = previousNode;
+    let nodeCount = 0;
+
+    // do this k number of times, basically
+    while(currentNode && nodeCount < k - 1) {
+        let placeholderNode = currentNode.next;
+        currentNode.next = previousNode;
+        previousNode = currentNode;
+        currentNode = placeholderNode;
+        nodeCount++;
+    }
+
+    // a little recursion magic here to keep adding a
+    // reversed tail of nodes on to the end of our existing last node.
+    if(currentNode !== null) {
+        lastNode.next = reverseInGroupsOfK(currentNode, k);
+    }
+
+    // previousNode ends up holding the first item that we
+    // swapped for at the top of this function
+    return previousNode;
+
 }
 
 let node5 = new LLNode(5, null);
