07e68cb2cde9907c2802b57fd97aad97e44de9ee
Jason Mauss <jason.mauss@gmail.com>
2023-03-25 23:27:48 -0700

    initial commit

---
+++ Problem_9_BinarySearch/JavaScript/Problem_9_BinarySearch.js
@@ -0,0 +1,17 @@
+// Solution to Formation's day 9 challenge/problem - BinarySearch
+// Implement binary search on an array. Return the index of the value if found and -1 otherwise.
+
+const binarySearch = (data, k) => {
+  
+    return -1;
+
+}
+
+
+
+
+
+
+// some test cases
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10,11,12,13,14], 5)); // 4
+console.log(binarySearch([1,2,3,4,6,7,8,9,10,11,12], 5)); // -1
