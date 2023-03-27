0efbddc354fee944c40b73d6b1a07bf5187232d6
Jason Mauss <jason.mauss@gmail.com>
2023-03-26 22:11:08 -0700

    initial commit

---
+++ Problem_10_MergeSort/JavaScript/Problem_10_MergeSort.js
@@ -0,0 +1,15 @@
+// Solution to Formation's day 10 problem/challenge
+// Merge Sort
+// Prompt: Implement merge sort on an array.
+
+const mergeSort = (data) => {
+  
+    return null;
+
+}
+
+
+
+// some test cases
+console.log(mergeSort([1,2,3,4,5])); // 
+console.log(mergeSort([5,6,2,4,8,6,1,3,6,9,0])); // 
