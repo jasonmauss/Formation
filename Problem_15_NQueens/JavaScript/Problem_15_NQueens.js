d47a8136967cba1d38efb5742ebfbdc4218eadee
Jason Mauss <jason.mauss@gmail.com>
2023-04-03 00:22:33 -0700

    initial commit

---
+++ Problem_15_NQueens/JavaScript/Problem_15_NQueens.js
@@ -0,0 +1,19 @@
+// Solution to Formation's Day 15 challenge/problem - NQueens
+// prompt: Given an N*N board, place N queens on the board such that none can attack each other. 
+// A queen can move an unlimited number of spaces in any direction: horizontal, vertical, or diagonal. 
+// Return an N*N matrix of ‘.’ and ‘Q’ characters, a ‘Q’ where ever a queen is positioned.
+
+const nQueens = (n) => {
+
+    return [];
+    
+}
+
+
+// some test cases
+console.log(nQueens(4));
+// returns: 
+
+
+console.log(nQueens(4));
+// returns: 
