6a7389d49c45586e3fe5d53dc8cf40198582c231
Jason Mauss <jason.mauss@gmail.com>
2023-04-02 23:52:18 -0700

    Adding Solution for JavaScript

--- Problem_14_GeneratePassword/JavaScript/Problem_14_GeneratePassword.js
+++ Problem_14_GeneratePassword/JavaScript/Problem_14_GeneratePassword.js
@@ -9,7 +9,36 @@
 
 const generatePassword = (characters, minlength, maxlength) => {
   
-    return [];
+    // store the passwords generated
+    const passwordCollection = [];
+
+    const charStack = [];
+    const charCounts = {};
+
+    const manageCharacterStack = () => {
+        if(charStack.length >= minlength && charStack.length <= maxlength) {
+            passwordCollection.push(charStack.join(''));
+        }
+
+        if(charStack.length === maxlength) return;
+
+        for(const char of characters) {
+            const charCount = charCounts[char] || 0;
+            if((charStack.length == 0 || charStack[charStack.length - 1] !== char) && charCount < 2) {
+                charStack.push(char);
+                charCounts[char] = charCount + 1;
+
+                manageCharacterStack();
+
+                charCounts[char]--;
+                charStack.pop();
+            }
+        }
+    };
+
+    manageCharacterStack();
+
+    return passwordCollection;
 
 }
 
@@ -19,9 +48,9 @@ const generatePassword = (characters, minlength, maxlength) => {
 // some test cases
 console.log(generatePassword(['a','b','c','d'], 4, 6));
 // returns
-// 
+// 60 items
 
 
 console.log(generatePassword(['f','a','s','w','d'], 4, 10));
 // returns
-// 
+// 126,890 items
