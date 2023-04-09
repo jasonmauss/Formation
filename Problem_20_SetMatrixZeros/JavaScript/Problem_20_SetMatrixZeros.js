374fbc876b5495369519093e81876f289d20cfcb
Jason Mauss <jason.mauss@gmail.com>
2023-04-09 00:27:36 -0700

    initial commit

---
+++ Problem_20_SetMatrixZeros/JavaScript/Problem_20_SetMatrixZeros.js
@@ -0,0 +1,28 @@
+// Solution to Formation's Day 20 challenge/problem - Set Matrix Zeros
+// Prompt: Given a rectangular matrix of numbers, find any position that is set to a zero 
+// in the input and then replace all values on its row and column with zeros. This must be 
+// done in-place, modifying the input matrix, not allocating any new space.
+/*
+For example:
+
+1 2 3
+4 5 6
+7 8 0
+
+Becomes:
+
+1 2 0
+4 5 0
+0 0 0
+*/
+
+const setMatrixZeros = (matrix) => {
+  
+    return [];
+
+}
+
+
+
+// some test cases
+console.log(setMatrixZeros([[1,2,3],[4,5,6],[7,8,0]])); // returns [[1,2,0],[4,5,0],[0,0,0]]
